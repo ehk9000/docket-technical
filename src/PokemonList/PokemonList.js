8629abfdbf289b1767adc5487640e3edb007142c
Erik <erikkonner@gmail.com>
2024-10-23 13:46:00 -0600

    Added article, export component

--- src/PokemonList/PokemonList.js
+++ src/PokemonList/PokemonList.js
@@ -1,24 +1,26 @@
-import React, {useEffect, useState} from React
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const PokemonList = () => {
-    const [pokemon, setPokemon] = useState([]);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [pokemon, setPokemon] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   return (
     <div>
       <h1>Random Pokémon</h1>
       <main>
-        {pokemon.map(p => (
-            <>
-            <h2>{p.name}</h2>
-            <img src={p.sprites.front_default} alt={p.name} />
-            </>
+        {pokemon.map((p) => (
+          <>
+            <article>
+              <h2>{p.name}</h2>
+              <img src={p.sprites.front_default} alt={p.name} />
+            </article>
+          </>
         ))}
-
-        </main>
+      </main>
     </div>
   );
 };
 
+export default PokemonList;
