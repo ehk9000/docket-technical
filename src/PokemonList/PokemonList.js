dff0f9f9079d48a61a0bd64e80e545367a234a6f
Erik <erikkonner@gmail.com>
2024-10-23 14:03:45 -0600

    API call to fetch three random pokemon

--- src/PokemonList/PokemonList.js
+++ src/PokemonList/PokemonList.js
@@ -6,6 +6,33 @@ const PokemonList = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        // Generate three random Pokémon IDs
+        const pokemonIds = Array.from(
+          { length: 3 },
+          () => Math.floor(Math.random() * 1025) + 1
+        );
+        const pokemonData = await Promise.all(
+          pokemonIds.map((id) =>
+            axios
+              .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+              .then((res) => res.data)
+          )
+        );
+        // set Pokemon response to state
+        setPokemon(pokemonData);
+      } catch (error) {
+        setError('Failed to fetch Pokémon');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPokemon();
+  }, []);
+
   return (
     <div>
       <h1>Random Pokémon</h1>
