9cb2d40bc05536f78cc115cb2a08775ba881fabe
Erik <erikkonner@gmail.com>
2024-10-24 11:01:55 -0600

    Added test for fetch to have been called three times

--- src/PokemonList/PokemonList.test.js
+++ src/PokemonList/PokemonList.test.js
@@ -1,11 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import PokemonList from './PokemonList';
+import axios from 'axios';
 
 jest.mock('axios');
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 test('renders error state initially', () => {
   render(<PokemonList />);
   const error = screen.getByText('Failed to fetch Pokémon');
 
   expect(error).toBeInTheDocument();
+});
+
+test('call pokemon api when the component is mounted', async () => {
+  axios.get.mockResolvedValueOnce();
+
+  axios.get.mockResolvedValueOnce();
+
+  axios.get.mockResolvedValueOnce();
+
+  render(<PokemonList />);
+
+  await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(3));
 });
